34670ebd5d4adc59d4584b0bcf559b117fcdb817
Chris Ovenden <>
2017-08-09 23:32:41 +0100

    add availbility field

--- cal/models.py
+++ cal/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+AVAILABILITY_OPTIONS = (
+    (u'unavailable', u'Unavailable'),
+    (u'available', u'Available'),
+    (u'unsure', u'Unsure'),
+)
 
 class TimeChunk(models.Model):
     start_time = models.DateTimeField()
@@ -9,6 +14,7 @@ class TimeChunk(models.Model):
     text = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
     last_changed = models.DateTimeField(auto_now=True)
+    availability = models.CharField(max_length=12, choices=AVAILABILITY_OPTIONS)
 
     def __str__(self):
         if self.text:
